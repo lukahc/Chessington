8998a6f54da02f1563aac363701a5f370e83d6b4
Luka <luka.cowley@softwire.com>
2022-11-28 11:46:54 +0000

    Added rook movement

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -5,12 +5,25 @@ namespace Chessington.GameEngine.Pieces
 {
     public class Rook : Piece
     {
-        public Rook(Player player)
-            : base(player) { }
+        public Rook(Player player) : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> availableMoves = new List<Square>();
+            var currentSquare = board.FindPiece(this);
+            for (int i = 0; i < 8; i++)
+            {
+                if (i != currentSquare.Col)
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row, i));
+                }
+
+                if (i != currentSquare.Row)
+                {
+                    availableMoves.Add(Square.At(i, currentSquare.Col));
+                }
+            }
+            return availableMoves;
         }
     }
 }
