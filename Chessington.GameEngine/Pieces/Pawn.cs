577fa9a4e8906e48f5087d493d3c4a5c79e7e735
Luka <luka.cowley@softwire.com>
2022-11-28 11:37:02 +0000

    Allowed pawns to move one square

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -5,12 +5,21 @@ namespace Chessington.GameEngine.Pieces
 {
     public class Pawn : Piece
     {
-        public Pawn(Player player) 
-            : base(player) { }
+        public Pawn(Player player) : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> availableMoves = new List<Square>();
+            var currentSquare = board.FindPiece(this);
+            if (this.Player == Player.White)
+            {
+                availableMoves.Add(Square.At(currentSquare.Row - 1, currentSquare.Col));
+            }
+            else
+            {
+                availableMoves.Add(Square.At(currentSquare.Row + 1, currentSquare.Col));
+            }
+            return availableMoves;
         }
     }
 }
